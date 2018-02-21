d31a3efa1f17c9a84e886087bc340b1aa0fd96cb
Satit Rianpit <rianpit@gmail.com>
2018-02-21 10:55:11 +0700

    initial bid select
    Signed-off-by: Satit Rianpit <rianpit@gmail.com>

---
+++ src/app/select-boxes/select-bid/select-bid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'po-select-bid',
+  templateUrl: './select-bid.component.html',
+  styles: []
+})
+export class SelectBidComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/select-boxes/select-boxes.module.ts
+++ src/app/select-boxes/select-boxes.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SelectBoxUnitsComponent } from './select-box-units/select-box-units.component';
 import { FormsModule } from '@angular/forms';
 import { SelectBudgetComponent } from './select-budget/select-budget.component';
+import { SelectBidComponent } from './select-bid/select-bid.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule
   ],
-  declarations: [SelectBoxUnitsComponent, SelectBudgetComponent],
+  declarations: [SelectBoxUnitsComponent, SelectBudgetComponent, SelectBidComponent],
   exports: [SelectBoxUnitsComponent]
 })
 export class SelectBoxesModule { }
