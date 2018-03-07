6ef0833cbc23eb5593983344718669adc307d720
peemppk <peemppk5538@gmail.com>
2018-03-07 11:33:33 +0700

    layout menu

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,13 @@
 
 export const environment = {
   production: false,
+  purchasingUrl: '/purchase',
+  planningUrl: 'http://mmis.moph.go.th/planning',
+  inventoryUrl: 'http://mmis.moph.go.th/inventory',
+  materialsUrl: 'http://mmis.moph.go.th/inventory',
+  reportUrl: 'http://mmis.moph.go.th/reports',
+  umUrl: 'http://mmis.moph.go.th/um',
+  contractsUrl: '/',
   apiUrl: 'http://localhost:3009',
   docUrl: 'http://api.mmis.moph.go.th/document',
   loginUrl: 'http://203.157.156.69/api/um/',
