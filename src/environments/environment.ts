d919aaea49e05a490e1bb818ee28e8a352c39f2a
peemppk <peemppk5538@gmail.com>
2018-03-07 11:22:30 +0700

    pull

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3009',
-  docUrl: 'http://localhost:3010',
-  loginUrl: 'http://localhost:3004',
+  docUrl: 'http://api.mmis.moph.go.th/document',
+  loginUrl: 'http://203.157.156.69/api/um/',
   homeUrl: 'http://mmis.moph.go.th/mmis',
   purchasingPoPrefix: 'PO',
   purchasingPrPrefix: 'PR'
