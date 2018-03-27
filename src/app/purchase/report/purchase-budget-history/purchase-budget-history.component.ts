16847a253f0f995c696d754441cb63281ce5fa10
peemppk <peemppk5538@gmail.com>
2018-03-27 13:43:09 +0700

    สร้างหน้ารายงานประวัติการใช้งบ

---
+++ src/app/purchase/report/purchase-budget-history/purchase-budget-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-budget-history',
+  templateUrl: './purchase-budget-history.component.html',
+  styles: []
+})
+export class PurchaseBudgetHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
