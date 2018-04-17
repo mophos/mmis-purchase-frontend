7040b2ae8e59bbd34c792063a7e4de1390ef5fa4
gun <gunnn_boy_hand@hotmail.com>
2018-04-17 09:22:13 +0700

    edit clear value order point

--- src/app/purchase/report/purchase-order/purchase-order.component.ts
+++ src/app/purchase/report/purchase-order/purchase-order.component.ts
@@ -61,6 +61,7 @@ export class PurchaseOrderComponent implements OnInit {
     const url = `${this.apiUrl}/report/list/purchase-trade-select/?token=${this.token}&${productIds}`;
     this.htmlPreview.showReport(url);
 
+    this.printProducts = [];
   }
 
   onDateStartChanged(event: IMyDateModel) {
